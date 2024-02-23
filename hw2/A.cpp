f1eee28f18bb2c2ef7b529dcce15caea1c0e0fd9
regentCracker <orenkovivo@gmail.com>
2024-02-23 13:24:43 +0200

    init

---
+++ hw2/A.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+   cout << "Hello world!" << endl;
+}
+
+int main(){
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   int t;
+   cin >> t;
+   for(int i = 0; i < t; i++)
+      solve();
+}
