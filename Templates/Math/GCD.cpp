7e9c5e708adc4a7651ba0686845cb70d0cbf45f8
Amit Fink <63642752+Amit-F@users.noreply.github.com>
2024-03-12 15:37:04 +0200

    Added GCD Algo
    Includes extended GCD Algo

--- Templates/Math/GCD.cpp
+++ Templates/Math/GCD.cpp
@@ -12,3 +12,40 @@ int gcd(int a, int b)
         return b;
     return gcd(b % a, a);
 }
+
+//  Extended Euclidean algorithm also finds integer coefficients x and y such that: ax + by = gcd(a, b) 
+
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+// Function for extended Euclidean Algorithm 
+int gcdExtended(int a, int b, int *x, int *y) 
+{ 
+    // Base Case 
+    if (a == 0) 
+    { 
+        *x = 0; 
+        *y = 1; 
+        return b; 
+    } 
+ 
+    int x1, y1; // To store results of recursive call 
+    int gcd = gcdExtended(b%a, a, &x1, &y1); 
+ 
+    // Update x and y using results of 
+    // recursive call 
+    *x = y1 - (b/a) * x1; 
+    *y = x1; 
+ 
+    return gcd; 
+} 
+
+// example call
+int main() 
+{ 
+    int x, y, a = 35, b = 15; 
+    int g = gcdExtended(a, b, &x, &y); 
+    cout << "GCD(" << a << ", " << b 
+         << ") = " << g << endl;
+    return 0; 
+} 
