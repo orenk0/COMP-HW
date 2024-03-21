10131845d695aa7a20796cd1dd7ba1e2be8a51c9
Amit Fink <63642752+Amit-F@users.noreply.github.com>
2024-03-21 20:49:56 +0200

    Create BFS_and_DFS.cpp

---
+++ Templates/Graphs/BFS_and_DFS.cpp
@@ -0,0 +1,31 @@
+vector<vector<int>> g;
+vector<bool> visible;
+void bfs(vector<int>& starts) {
+    for(int x : starts)
+       visible[x]=true;
+    vector<int> cur_level=starts;
+    int dist=0;
+    while (!cur_level.empty()) {
+        vector<int> next_level;
+        for(int f : cur_level) {
+            for(int s : g[f]) {
+                if (visible[s]) continue;
+                visible[s]=true;
+                next_level.push_back(s);
+            }
+        }
+	cur_level = next_level;
+        dist++;
+    }
+}
+
+
+vector<vector<int>> g; vector<bool> visible;
+void dfs(int cur) {
+    visible[cur] = true;
+    for(int s : g[cur]) {
+        if(visible[s]) continue;
+        dfs(s);
+    }
+}
+
